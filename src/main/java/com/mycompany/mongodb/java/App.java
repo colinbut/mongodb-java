b2c758fe8b8ce3e4e412d18504cc1b930292a6d0
Colin But <colinbut@users.noreply.github.com>
2017-11-05 11:42:19 +0000

    Create and Read

--- src/main/java/com/mycompany/mongodb/java/App.java
+++ src/main/java/com/mycompany/mongodb/java/App.java
@@ -10,6 +10,10 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
@@ -18,6 +22,43 @@ public class App {
 
         MongoCollection<Document> usersCollection = testDb.getCollection("users");
 
+        // drop collection
+        usersCollection.drop();
+
+        // Insert
+        insertMultiples(usersCollection);
+
+        // Query
+        queryResults(usersCollection);
+
+        // Update
+
+        // Delete
+
         mongoClient.close();
     }
+
+    public static void insertMultiples(MongoCollection<Document> usersCollection) {
+        Document jamie = new Document("name", "Jamie").append("age", 27);
+        Document andy = new Document("name", "Andy").append("age", 12);
+        Document steven = new Document("name", "Steven").append("age", 18);
+        Document david = new Document("name", "David").append("age", 29);
+
+        usersCollection.insertMany(Arrays.asList(jamie, andy, steven, david));
+    }
+
+    public static void queryResults(MongoCollection<Document> usersCollection) {
+        List<Document> users = usersCollection.find().into(new ArrayList<Document>());
+        for (Document user : users) {
+            System.out.println(user);
+        }
+
+        Document user = usersCollection.find(new Document("name", "Jamie")).first();
+        System.out.println("User:" + user);
+
+        Document jamie = usersCollection.find(new Document("name", "Jamie")
+            .append("age", new Document("$gte", 25))).first();
+        System.out.println(jamie);
+    }
+
 }
