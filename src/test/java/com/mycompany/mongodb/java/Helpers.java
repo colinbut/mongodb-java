0187268cb90767342e8f18b8288b56d36a1e2c92
Colin But <colinbut@users.noreply.github.com>
2017-11-06 21:24:13 +0000

    Added a helper method to print json nicely

---
+++ src/test/java/com/mycompany/mongodb/java/Helpers.java
@@ -0,0 +1,31 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2017 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.mongodb.java;
+
+import org.bson.Document;
+import org.bson.codecs.DocumentCodec;
+import org.bson.codecs.EncoderContext;
+import org.bson.json.JsonMode;
+import org.bson.json.JsonWriter;
+import org.bson.json.JsonWriterSettings;
+
+import java.io.StringWriter;
+
+public class Helpers {
+
+    public static void printJson(Document document) {
+        JsonWriter jsonWriter = new JsonWriter(new StringWriter(),
+            new JsonWriterSettings(JsonMode.SHELL, false));
+
+        new DocumentCodec().encode(jsonWriter,
+            document,
+            EncoderContext.builder().isEncodingCollectibleDocument(true).build());
+
+        System.out.println(jsonWriter.getWriter());
+
+        System.out.flush();
+    }
+}
